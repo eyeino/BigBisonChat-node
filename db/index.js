a5bf586938e9f0265a421bca6923c70e7938ca74
Ian MacFarlane <ian.ross.macfarlane@gmail.com>
2021-03-29 22:48:02 -0400

    add ssl key for pg on heroku

--- db/index.js
+++ db/index.js
@@ -9,6 +9,9 @@ let pool = null;
 if (process.env.DATABASE_URL) {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   })
 } else {
   pool = new Pool();
