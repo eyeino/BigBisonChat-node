02914b79e7f7d1bcab8e312eef54a626e24d64b4
Ian MacFarlane <1900909+iannotian@users.noreply.github.com>
2021-07-04 16:31:22 -0400

    change bootstrap func to iife

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { initServer } from "./app/server";
 
-function bootstrap() {
+(function main() {
   const server = initServer();
 
   const port = Number(process.env.PORT) || 8080;
@@ -8,6 +8,4 @@ function bootstrap() {
   server.listen(port, async () => {
     console.log(`server listening on port ${port}`);
   });
-}
-
-bootstrap();
+})();
