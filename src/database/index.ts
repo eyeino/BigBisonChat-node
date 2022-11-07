efa788593d0e5f458d017ccb7c094925fe4bcb10
Ian MacFarlane <1900909+iannotian@users.noreply.github.com>
2022-11-06 23:03:24 -0800

    add basic fns

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,94 +1,50 @@
-import { PrismaClient, messages, users } from '@prisma/client';
+import { PrismaClient, Message, User, Room } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-// messages between two users
-export const getConversation = async (
-  userId1: number,
-  userId2: number
-): Promise<messages[]> => {
-  return await prisma.messages.findMany({
+export const getRoomsForUser = async (user: User): Promise<Room[]> => {
+  return await prisma.room.findMany({
     where: {
-      OR: [
-        { sender: userId1, recipient: userId2 },
-        { sender: userId2, recipient: userId1 },
-      ],
+      members: {
+        some: {
+          id: {
+            equals: user.id,
+          },
+        },
+      },
+    },
+  });
+};
+
+export const getMessagesForRoom = async (room: Room): Promise<Message[]> => {
+  return await prisma.message.findMany({
+    where: {
+      roomId: room.id,
     },
     orderBy: {
-      created_at: 'desc',
+      createdAt: 'desc',
     },
   });
 };
 
-// last messages where user is either sender or recipient
-export const getConversations = async (userId: number): Promise<messages[]> => {
-  return await prisma.messages.findMany({
-    where: {
-      OR: [{ sender: userId }, { recipient: userId }],
-      AND: [],
-    },
-    distinct: ['sender', 'recipient'],
-    orderBy: {
-      created_at: 'desc',
-    },
-  });
-};
-
-// get userId from openid sub
-export const getUserBySub = async (
-  open_id_sub: string
-): Promise<users | null> => {
-  return await prisma.users.findUnique({
-    where: { open_id_sub },
-  });
-};
-
-export const getUserByUsername = async (
-  username: string
-): Promise<users | null> => {
-  return await prisma.users.findUnique({
-    where: { username },
-  });
-};
-
-export const upsertUser = async (
-  open_id_sub: string,
-  username: string,
-  avatar_url?: string,
-  first_name?: string,
-  last_name?: string,
-  email?: string
-): Promise<users> => {
-  const user = await getUserBySub(open_id_sub);
-  if (user) return user;
-
-  return await prisma.users.create({
-    data: {
-      username,
-      avatar_url,
-      first_name,
-      last_name,
-      email,
-      open_id_sub,
-    },
-  });
-};
-
-export const searchUsers = async (query: string): Promise<users> => {
-  return await prisma.$queryRaw`SELECT * FROM users WHERE username LIKE ${query}%`;
-};
-
-// create message
 export const createMessage = async (
-  senderId: number,
-  recipientId: number,
-  body: string
-): Promise<messages> => {
-  return await prisma.messages.create({
+  body: string,
+  sender: User,
+  room: Room
+): Promise<Message> => {
+  return await prisma.message.create({
     data: {
       body,
-      recipient: recipientId,
-      sender: senderId,
+      senderId: sender.id,
+      roomId: room.id,
     },
   });
+};
+
+export const findUsersWithUsernameLike = async (
+  query: string
+): Promise<User[]> => {
+  return await prisma.$queryRaw<
+    User[]
+  >`SELECT * FROM User WHERE username LIKE ${query};`;
 };
